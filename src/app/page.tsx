61233e6fd1942d1298357044e994602a1e318c3c
Mike Kamilos <64214237+mikam-dev@users.noreply.github.com>
2024-02-04 22:20:52 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Hero from "@/components/layout/Hero";
 
 export default function Home() {
   return (
-    <main className="flex bg-background min-h-[100svh] pt-[8svh] pb-8 flex-col items-center justify-between">
+    <main className="flex bg-background min-h-[92svh] pb-8 flex-col items-center justify-between">
       <Hero>
         <h1 className="text-6xl font-bold">Clean.js</h1>
-        <p className="text-2xl">This is a Next.js + Tailwind CSS starter template.</p>
+        <p className="text-2xl">This is a Next.js + <strong>shadcn/ui</strong> starter template.</p>
       </Hero>
     </main>
   );
